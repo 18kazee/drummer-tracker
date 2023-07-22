4c394d53dab3fd9f9862904c0a1ee4a8bd0b1f24
18kazee <18kazee2415@gmail.com>
2023-07-23 08:48:13 +0900

    Fix: 本番環境のメーラーをSEND GRIDに変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,19 +63,18 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "drummer_tracker_production"
 
   config.action_mailer.perform_caching = false
-
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { :host => 'drummer-tracker-85a20c07e028.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :user_name      => ENV['GMAIL_USERNAME'],
-    :password       => ENV['GMAIL_PASSWORD'],
+    :user_name => ENV['SENDGRID_USER_NAME'], 
+    :password => ENV['SENDGRID_API_KEY'],
     :domain => "heroku.com",
-    :address => "smtp.gmail.com",
+    :address => "smtp.sendgrid.net",
     :port => 587,
     :authentication => :plain,
     :enable_starttls_auto => true
   }
-
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
