30e9304d4499b80f8a88e35ff10f66cf0f081136
18kazee <18kazee2415@gmail.com>
2023-09-20 02:38:45 +0900

    Fix: mailerの修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
   ActionMailer::Base.smtp_settings = {
     :user_name => ENV['SENDGRID_USER_NAME'], 
     :password => ENV['SENDGRID_API_KEY'],
-    :domain => "heroku.com",
+    :domain => "drummer-tracker.com",
     :address => "smtp.sendgrid.net",
     :port => 587,
     :authentication => :plain,
